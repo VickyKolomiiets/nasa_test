ecbcbc15cd17f368a7d52cbf349d180cb112eed8
VickyKolomiiets <Vicky@example.com>
2023-07-21 01:51:39 +0300

    Layout setup

--- src/components/NeoList/NeoList.tsx
+++ src/components/NeoList/NeoList.tsx
@@ -2,17 +2,99 @@ import React, { useState, useEffect } from "react";
 import NeoListItem from "./NeoListItem";
 import { getNEOs } from "../../services/nasaAPI";
 
+import AppBar from "@mui/material/AppBar";
+import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import CssBaseline from "@mui/material/CssBaseline";
+import Grid from "@mui/material/Grid";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Link from "@mui/material/Link";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
 const NEOList = () => {
-  const [neoData, setNeoData] = useState<any>([]);
-  const [startIndex, setStartIndex] = useState(0);
+  const defaultTheme = createTheme();
 
-  const fetchData = async (startDate, endDate) => {
-    const data = await getNEOs(startDate, endDate);
-    // Aggregate the data as needed here before setting it
-    return data;
-  };
-
-  return <div>{/* <NeoListItem key={index} item={item} /> */}</div>;
+  return (
+    <div>
+      <ThemeProvider theme={defaultTheme}>
+        <CssBaseline />
+        <AppBar position="relative">
+          <Toolbar>
+            <Typography variant="h6" color="inherit" noWrap>
+              Album layout
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <main>
+          {/* Hero unit */}
+          <Box
+            sx={{
+              bgcolor: "background.paper",
+              pt: 8,
+              pb: 6,
+            }}
+          >
+            <Container maxWidth="sm">
+              <Typography
+                component="h1"
+                variant="h2"
+                align="center"
+                color="text.primary"
+                gutterBottom
+              >
+                Album layout
+              </Typography>
+              <Typography
+                variant="h5"
+                align="center"
+                color="text.secondary"
+                paragraph
+              >
+                Something short and leading about the collection below—its
+                contents, the creator, etc. Make it short and sweet, but not too
+                short so folks don&apos;t simply skip over it entirely.
+              </Typography>
+              <Stack
+                sx={{ pt: 4 }}
+                direction="row"
+                spacing={2}
+                justifyContent="center"
+              >
+                <Button variant="contained">Main call to action</Button>
+                <Button variant="outlined">Secondary action</Button>
+              </Stack>
+            </Container>
+          </Box>
+          <Container sx={{ py: 8 }} maxWidth="md">
+            {/* End hero unit */}
+            <Grid container spacing={4}></Grid>
+          </Container>
+        </main>
+        {/* Footer */}
+        <Box sx={{ bgcolor: "background.paper", p: 6 }} component="footer">
+          <Typography variant="h6" align="center" gutterBottom>
+            Footer
+          </Typography>
+          <Typography
+            variant="subtitle1"
+            align="center"
+            color="text.secondary"
+            component="p"
+          >
+            Something here to give the footer a purpose!
+          </Typography>
+        </Box>
+        {/* End footer */}
+      </ThemeProvider>
+    </div>
+  );
 };
 
 export default NEOList;
--- src/components/NeoList/NeoListItem.tsx
+++ src/components/NeoList/NeoListItem.tsx
@@ -1,7 +1,55 @@
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
 import React from "react";
 
 const NeoListItem = ({ item }: any) => {
-  return <></>;
+  return (
+    <Card
+      sx={{
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <CardMedia
+        component="div"
+        sx={{
+          // 16:9
+          pt: "56.25%",
+        }}
+        image="https://source.unsplash.com/random?wallpapers"
+      />
+      <CardContent sx={{ flexGrow: 1 }}>
+        <Typography gutterBottom variant="h5" component="h2">
+          Heading
+        </Typography>
+        <Typography>Max Diameter: {item.maxDiameter}</Typography>
+
+        <Typography>Hazardous NEOs: {item.hazardousNEOs}</Typography>
+        <Typography>Closest NEO: {item.closestNEO}</Typography>
+        <Typography>Fastest NEO: {item.fastestNEO}</Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">View</Button>
+        <Button size="small">Edit</Button>
+      </CardActions>
+    </Card>
+
+    // <Card variant="outlined">
+    //   <div style={{ backgroundColor: item.isHazardous ? "red" : "white" }}>
+    //     <p>Max Diameter: {item.maxDiameter}</p>
+    //     <p>Hazardous NEOs: {item.hazardousNEOs}</p>
+    //     <p>Closest NEO: {item.closestNEO}</p>
+    //     <p>Fastest NEO: {item.fastestNEO}</p>
+    //   </div>
+    // </Card>
+  );
 };
 
 export default NeoListItem;
